a618cf0ab3b1e7136aa19c636d330ed286f5fefe
Juan Meriño <juanmerinoortiz24@gmail.com>
2024-01-20 15:46:54 -0500

    Resuelvo los ejercicios de fundamentos de javascript

--- M05 Fundamentos Javascript/ejercicios/08.js
+++ M05 Fundamentos Javascript/ejercicios/08.js
@@ -3,6 +3,9 @@ function esNumeroEntero(numero) {
   // Verifica si este es un número entero o no.
   // Returna true si lo es, de lo contrario, retorna false.
   // Tu código:
+  if(typeof numero === 'number') {
+    return numero % 1 === 0;
+  }else return false;
 }
 
 module.exports = esNumeroEntero;
--- M05 Fundamentos Javascript/ejercicios/11.js
+++ M05 Fundamentos Javascript/ejercicios/11.js
@@ -5,6 +5,14 @@ function esMayorDeEdad(fechaNacimiento) {
   // Devuelve true si lo es, caso contrario, retorna false.
   // PISTA: Investiga el método getFullYear() de la clase Date para obtener el año actual.
   // Tu código:
+  var año = fechaNacimiento.getFullYear();
+  var añoPrecente = new Date().getFullYear();
+
+  if(añoPrecente - año > 18){
+    return true;
+  }else {
+    return false;
+  }
 }
 
 module.exports = esMayorDeEdad;
